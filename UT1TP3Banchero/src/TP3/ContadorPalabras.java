b5e7d5f41802310c422a4ef2bdfce8447c38a0cd
Facundo Banchero <facubanchero01@gmail.com>
2025-03-31 17:51:14 -0300

    Corrijo ubicación de ContadorPalabras y Principal dentro de UT1TP3Banchero

--- UT1TP3Banchero/src/TP3/ContadorPalabras.java
+++ UT1TP3Banchero/src/TP3/ContadorPalabras.java
@@ -41,4 +41,96 @@ public class ContadorPalabras {
 		// Devuelvo el numero de palabras
 		return contadorPalabras;
 	}
+	
+	public int contarVocales(String frase) {
+		if (frase == null || frase.isEmpty()) {
+			return 0;
+		}
+		
+		// Variable para contar las vocales
+		int contadorVocales = 0;
+		
+		// Convierto a minusculas para que se me haga mas facil verificar.
+		frase = frase.toLowerCase();
+		
+		for (int i = 0; i < frase.length(); i++) {
+			char caracterActual = frase.charAt(i);
+			
+			// Verificacion de si es vocal.
+			if (caracterActual == 'a' || caracterActual == 'e' || caracterActual == 'i' || caracterActual == 'o' || caracterActual == 'u') {
+				contadorVocales = contadorVocales + 1;
+			}
+		}
+		
+		return contadorVocales;
+	}
+	
+	public int contarConsonantes(String frase) {
+		if(frase == null || frase.isEmpty()) {
+			return 0;
+		}
+		
+		int contadorConsonantes = 0;
+		
+		frase = frase.toLowerCase();
+		
+		for (int i = 0; i < frase.length(); i++) {
+			char caracterActual = frase.charAt(i);
+			
+			// Necesito verificar si es una letra
+			boolean esLetra = Character.isLetter(caracterActual);
+			
+			// Neceisto verificar si no es una vocal
+			boolean noEsVocal = (caracterActual != 'a' && caracterActual != 'e' && caracterActual != 'i' && caracterActual != 'o' && caracterActual != 'u');
+			
+			if (esLetra && noEsVocal) {
+				contadorConsonantes = contadorConsonantes + 1;
+			}
+		}
+		
+		return contadorConsonantes;
+	}
+	
+	public int contarPalabrasMayoresA(String frase, int largoMinimo) {
+		if (frase == null || frase.trim().isEmpty()) {
+			return 0;
+		}
+		
+		int contadorPalabrasLargas = 0;
+		
+		// Variables auxiliares
+		boolean dentroDeUnaPalabra = false;
+		int largoActual = 0;
+		
+		// Agrego un espacio al final para poder con la ultima palabra
+		frase = frase + " ";
+		
+		for (int i = 0; i < frase.length(); i++) {
+			char caracterActual = frase.charAt(i);
+			
+			// Si no es un espacio y estoy en una palabra
+			if (caracterActual != ' ') {
+				if (!dentroDeUnaPalabra) {
+					dentroDeUnaPalabra = true;
+					largoActual = 0;
+				}
+				
+				// Aumento el largo de la palabra actual por 1
+				largoActual = largoActual + 1;
+			}
+			
+			// Si encuentro un espacio y estoy en una palabra
+			else if (dentroDeUnaPalabra) {
+				// Termino la palabra actual
+				dentroDeUnaPalabra = false;
+				
+				// Y verifico si la palabra es mas larga que el minimo
+				if (largoActual > largoMinimo) {
+					contadorPalabrasLargas = contadorPalabrasLargas + 1;
+				}
+			}
+		}
+		
+		return contadorPalabrasLargas;
+	}
 }
--- UT1TP3Banchero/src/TP3/Principal.java
+++ UT1TP3Banchero/src/TP3/Principal.java
@@ -20,10 +20,17 @@ public class Principal {
 			int palabras = contador.contarPalabras(frase);
 			
 			// Muestro resultados de la frase y las palabras
-			System.out.println(frase);
-			System.out.println(palabras);
-			System.out.println();
+			System.out.println("Frase: " + frase);
+			System.out.println("Numero de palabras: " + palabras);
 			
+			// Cuento vocales, consonantes y palabras largas
+			int vocales = contador.contarVocales(frase);
+			int consonantes = contador.contarConsonantes(frase);
+			int palabrasLargas = contador.contarPalabrasMayoresA(frase, 4);
+			
+			System.out.println("Numero de vocales: " + vocales);
+			System.out.println("Numero de consonantes: " + consonantes);
+			System.out.println("Palabras con mas de 4 caracteres: " + palabrasLargas);
 		}
 	}
 
